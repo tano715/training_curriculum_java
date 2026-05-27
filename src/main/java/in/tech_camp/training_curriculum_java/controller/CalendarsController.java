8e86aa750492a7e37f47d8d12e361afc47d48036
tano715 <takashirotano715@gmail.com>
2026-05-27 13:59:37 +0900

    Issue5

--- src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
+++ src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
@@ -32,7 +32,7 @@ public class CalendarsController {
     model.addAttribute("planForm", new PlanForm());
     List<Map<String, Object>> weekDays = getWeek();
     model.addAttribute("weekDays", weekDays);
-    return "calendars/index";
+    return "redirect:/";
   }
 
   // 予定の保存
